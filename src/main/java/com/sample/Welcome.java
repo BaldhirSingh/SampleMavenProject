a7837f43e5aa1dff74f879edb913aca7dca5b6f1
Baldhir Now 7 <baldhirsingh7@gmail.com>
2024-05-30 00:58:35 +0530

    first file

---
+++ src/main/java/com/sample/Welcome.java
@@ -0,0 +1,12 @@
+package com.sample;
+
+public class Welcome {
+	//
+	String greet;
+	
+	public String welcomeUser(String greet) {
+		System.out.println("Print Greet Message" + greet);
+		return greet;
+	}
+
+}
